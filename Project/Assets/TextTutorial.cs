bbf8bd61c59a9c4db56db3b0b6411cc92ea4c562
Hứa Trường Khả <htkha1203@gmail.com>
2024-09-18 20:13:16 +0700

    tutorial

---
+++ Project/Assets/TextTutorial.cs
@@ -0,0 +1,50 @@
+using TMPro;
+using UnityEngine;
+
+namespace InfiniteZombies.Interface
+{
+    public class TextTutorial : ElementText
+    {
+        #region FIELDS SERIALIZED
+        
+        [Header("References")]
+        
+        [Tooltip("Tutorial prompt text.")]
+        [SerializeField]
+        private TextMeshProUGUI prompt;
+
+        [Tooltip("Tutorial text.")]
+        [SerializeField]
+        private TextMeshProUGUI tutorial;
+
+        #endregion
+
+        #region UNITY
+
+        protected override void Awake()
+        {
+            //Base.
+            base.Awake();
+            //Enable the prompt by default.
+            prompt.enabled = true;
+            //Disable the tutorial by default.
+            tutorial.enabled = false;
+        }
+
+        #endregion
+
+        #region METHODS
+
+        protected override void Tick()
+        {
+            //Get whether we should be showing the tutorial text, or not.
+            bool isVisible = playerCharacter.IsTutorialTextVisible();
+            //Hide the prompt if the tutorial is visible.
+            prompt.enabled = !isVisible;
+            //Show the tutorial if needed.
+            tutorial.enabled = isVisible;
+        }
+
+        #endregion
+    }
+}
